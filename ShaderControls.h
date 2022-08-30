20ae942a10c77c9de7ebec7711d7c3a8cdb73dd1
mchagneux <mathis.chagneux@telecom-paris.fr>
2022-08-30 10:48:40 +0200

    starting to add windows to select the shader

---
+++ ShaderControls.h
@@ -0,0 +1,17 @@
+#include <JuceHeader.h>
+#include "Identifiers.h"
+
+
+
+// class ShaderControls : public DocumentWindow {
+//     public: 
+//         ShaderControls(const ValueTree& sState): shaderState(sState) 
+//         {
+
+//         };
+//         ~ShaderControls() override;
+//     private:
+//         ValueTree shaderState; 
+
+//         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ShaderControls)
+// };
